fc7bb6540be790aef42ad30aa3229dd1efab4408
meriem <meriemismahlarbi@gmail.com>
2018-06-09 18:18:31 +0200

    create_user update

--- create_user.php
+++ create_user.php
@@ -1,22 +1,35 @@
+#!/usr/bin/php
 <?php
-$servername = "localhost";
+
+$servername = "37.59.53.29";
 $username_db = "ismahene";
 $userpwd_db = "PpfbfQrCmsfUI3d7";
 $db_name = "go_ismahene";
 
+
 try
 {
-	$db = new PDO("mysql:host=$servername; dbname=$db_name", $username_db, $userpwd_db);
-	$q1 = $_GET["q1"];
+
+	$con = mysqli_connect($servername, $username_db, $userpwd_db, $db_name );
+	echo"you've connected successfully\n";
+        $q1 = $_GET["q1"];
 	$q2 = $_GET["q2"];
 	$q3 = $_GET["q3"];
 	$q4 = $_GET["q4"];
 	$q5 = $_GET["q5"];
-	$sql = "INSERT INTO reponsebot(q1, q2, q3, q4, q5) VALUES ($q1, $q2, $q3, $q4, $q5)";
+print("inserting...\n");
+if (!$rqst = $con->query("INSERT INTO reponsesbot(q1, q2, q3, q4, q5) VALUES('$q1', '$q2', '$q3', '$q4', '$q5')"))
+    if(!($rqst))
+        echo "nope\n";
+    else
+        echo "done\n";
+
 }
 catch(PDOException $e)
 {
-	print($e->getMessage()."<br/>");
+	print($e->getMessage()."\n");
 	die();
 }
 ?>
+
+affiche ca!
